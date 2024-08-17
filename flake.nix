f1ecd2ad6a6fde896f2346ce394f817f71e229d3
Codebase-Torben <dauben_01_verehrer@icloud.com>
2024-08-17 16:57:53 +0000

    flake.nix aktualisieren
    Flush gut

--- flake.nix
+++ flake.nix
@@ -3,10 +3,6 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-Release.url = "github:NixOS/nixpkgs/nixos-24.05";
-    disko = {
-      url = "github:nix-community/disko";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     home-manager = {
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -18,28 +14,23 @@
   };
   outputs = {
     self,
-    disko,
     nixpkgs,
     nixpkgs-Release,
     home-manager,
     home-manager-Release,
   }: {
     nixosConfigurations = {
-      #################
-      # GENERIC NIXOS #
-      #################
+      # NixOS
       nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           home-manager.nixosModules.home-manager
           ./configuration.nix
-          ./hardware/nvidia-off.nix
+          ./hardware/macbook.nix
           ./modules/chronyPublic.nix
           ./desktop/gnome.nix
-          ./user/desktop/me.nix
-          ./server/adguard.nix
-          ./server/virtual.nix
-          {networking.hostName = "nixos";}
+          ./user/torben/flake.nix
+          {networking.hostName = "nixbook";}
         ];
       };
     };
