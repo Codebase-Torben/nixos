f0e10f17a00843987ff3a9c5a2e279b1b3a24706
Codebase-Torben <dauben_01_verehrer@icloud.com>
2024-08-21 11:41:12 +0000

    Update global.nix
    meow Dickels

--- alias/global.nix
+++ alias/global.nix
@@ -1,9 +1,9 @@
 {config, ...}: {
-  # Shell Aliase #
+  # Shell Aliase
   environment = {
     shellAliases = {
-      # NixOS Aliase
-      "nix.build" = ''
+    # NixOS Aliase
+      "nix.build" = ''            
         cd /etc/nixos &&\
         env sudo -v &&\
         sudo alejandra --quiet . &&\
@@ -15,19 +15,19 @@
         export HSTNM="$(hostname)" ;\
         echo "############# ---> NIXOS-REBUILD NixOS [$HSTNM-$ZTSTMPL] <--- ##################"
         sudo nixos-rebuild boot -v --fallback --flake "/etc/nixos/.#$HSTNM" -p "$HSTNM-$ZTSTMPL" '';
-      "nix.repair" = ''
+      "nix.repair" = ''            
         cd /etc/nixos &&\
         env sudo -v &&\
         sudo nix-store --gc ;\
         sudo nix-store --verify --check-contents --repair'';
-      "nix.clean" = ''
+      "nix.clean" = ''            
         cd /etc/nixos &&\
         env sudo -v &&\
-        sudo nix-env --delete-generations --profile /nix/var/nix/profiles/system 12d ;\
+        sudo nix-env --delete-generations --profile /nix/var/nix/profiles/system 13d ;\
         sudo nix-collect-garbage --delete-older-than 12d ;\
         sudo nix-store --gc ;\
         sudo nix-store --optimise '';
-      "nix.cleanfull" = ''
+      "nix.cleanfull" = ''            
         cd /etc/nixos &&\
         sudo -v &&\
         sudo rm /boot/loader/entries/nixos* ;\
@@ -39,57 +39,57 @@
         sudo nix-store --gc ;\
         sudo nix-store --optimise '';
       #"nix.cacheall" = ''
-        #cd /etc/nixos &&\
-        #nix.update ;\
-        #cd && mkdir -p cache && cd cache &&\
-        #nixos-rebuild build -v --fallback --flake /etc/nixos/#FLAKE ;\
+      #cd /etc/nixos &&\
+      #nix.update ;\
+      #cd && mkdir -p cache && cd cache &&\
+      #nixos-rebuild build -v --fallback --flake /etc/nixos/#FLAKE ;\
       #"nix.gitpush" = ''
-        #cd /etc/nixos &&\
-        #env sudo -v &&\
-        #host github.com ;\
-        #host api.github.com ;\
-        #host cache.nixos.org ;\
-        #sudo alejandra --quiet . &&\
-        #sudo chown -R NAME:GROUP .git &&\
-        #git reset &&\
-        #git add . &&\
-        #git commit -S -m update ;\
-        #git fsck --full &&\
-        #git gc --aggressive &&\
-        #git push --force '';
+      #cd /etc/nixos &&\
+      #env sudo -v &&\
+      #host github.com ;\
+      #host api.github.com ;\
+      #host cache.nixos.org ;\
+      #sudo alejandra --quiet . &&\
+      #sudo chown -R NAME:GROUP .git &&\
+      #git reset &&\
+      #git add . &&\
+      #git commit -S -m update ;\
+      #git fsck --full &&\
+      #git gc --aggressive &&\
+      #git push --force '';
       #"nix.test" = ''
-        #cd /etc/nixos &&\
-        #env sudo -v &&\
-        #sudo alejandra --quiet . ;\
-        #git reset ;\
-        #git add . ;\
-        #git commit -S -m update ;\
-        #sudo nixos-rebuild dry-activate --flake /etc/nixos/.#$(hostname)'';
+      #cd /etc/nixos &&\
+      #env sudo -v &&\
+      #sudo alejandra --quiet . ;\
+      #git reset ;\
+      #git add . ;\
+      #git commit -S -m update ;\
+      #sudo nixos-rebuild dry-activate --flake /etc/nixos/.#$(hostname)'';
       #"nix.update" = ''
-        #cd /etc/nixos &&\
-        #env sudo -v &&\
-        #sudo alejandra --quiet . &&\
-        #sudo chown -R me:users .git &&\
-        #git reset &&\
-        #git add . &&\
-        #git commit -S -m update ;\
-        #git fsck --full &&\
-        #git gc --aggressive &&\
-        #sudo nix flake lock --update-input nixpkgs --update-input nixpkgs-Release --update-input home-manager ;\
-        #sudo alejandra --quiet .'';
+      #cd /etc/nixos &&\
+      #env sudo -v &&\
+      #sudo alejandra --quiet . &&\
+      #sudo chown -R me:users .git &&\
+      #git reset &&\
+      #git add . &&\
+      #git commit -S -m update ;\
+      #git fsck --full &&\
+      #git gc --aggressive &&\
+      #sudo nix flake lock --update-input nixpkgs --update-input nixpkgs-Release --update-input home-manager ;\
+      #sudo alejandra --quiet .'';
       #"nix.switch" = ''
-        #nix.build ;\
-        #sudo nixos-rebuild switch --flake "/etc/nixos/.#$HSTNM" -p "$HSTNM-$ZTSTMPL"'';
+      #nix.build ;\
+      #sudo nixos-rebuild switch --flake "/etc/nixos/.#$HSTNM" -p "$HSTNM-$ZTSTMPL"'';
       #"nix.boot" = ''
-        #nix.build &&\
-        #sudo nixos-rebuild boot -v --fallback --install-bootloader '';
+      #nix.build &&\
+      #sudo nixos-rebuild boot -v --fallback --install-bootloader '';
       #"nix.all" = ''
-        #nix.update ;\
-        #nix.build ;\
-        #echo "############# ---> NIXOS-REBUILD **all** NixOS [$HSTNM-$ZTSTMPL] <--- ##########"
-        #sudo nixos-rebuild boot -v --fallback --flake /etc/nixos/#nixos         -p "nixos-$ZTSTMPL" ;\
-        #sudo nixos-rebuild boot -v --fallback --flake /etc/nixos/#nixos-console -p "nixos-console-$ZTSTMPL" ;\
-        #sudo nixos-rebuild boot -v --fallback --flake /etc/nixos/#$HSTNM        -p "$HSTNM-$ZTSTMPL" '';
+      #nix.update ;\
+      #nix.build ;\
+      #echo "############# ---> NIXOS-REBUILD **all** NixOS [$HSTNM-$ZTSTMPL] <--- ##########"
+      #sudo nixos-rebuild boot -v --fallback --flake /etc/nixos/#nixos         -p "nixos-$ZTSTMPL" ;\
+      #sudo nixos-rebuild boot -v --fallback --flake /etc/nixos/#nixos-console -p "nixos-console-$ZTSTMPL" ;\
+      #sudo nixos-rebuild boot -v --fallback --flake /etc/nixos/#$HSTNM        -p "$HSTNM-$ZTSTMPL" '';
     # Globale Aliase
       n = "nano";
       cro = "systemctl status chronyd ; chronyc tracking ; chronyc sources ; chronyc sourcestats ; sudo chronyc authdata ; sudo chronyc serverstats";
