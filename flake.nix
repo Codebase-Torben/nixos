38212b3a7dec88cbe32f235f12a7ee06eaba9267
Codebase-Torben <dauben_01_verehrer@icloud.com>
2024-08-23 21:02:27 +0000

    flake.nix aktualisieren

--- flake.nix
+++ flake.nix
@@ -34,7 +34,7 @@
           #./hardware/macbook.nix
           ./desktheme/gnome.nix
           ./users/torben.nix
-	        ./modules/office.nix
+          ./modules/office.nix
           #./modules/timeserver.nix
           {networking.hostName = "nixbook";}
         ];
