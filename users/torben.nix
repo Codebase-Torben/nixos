f0707fd3743f1ac2acfbdbf351d7037bd4a3b980
Codebase-Torben <dauben_01_verehrer@icloud.com>
2024-09-07 10:33:48 +0000

    Luna v1.06 Mail Edition

--- users/torben.nix
+++ users/torben.nix
@@ -1,4 +1,4 @@
-# Konfiguration des Userhome mit home-manger 
+# Konfiguration des Userhome mit home-manger
 {
   config,
   pkgs,
@@ -6,24 +6,24 @@
   home-manager,
   ...
 }: {
-# Benutzer
-users.users.torben = {
-  initialHashedPassword = "$y$j9T$SSQCI4meuJbX7vzu5H.dR.$VUUZgJ4mVuYpTu3EwsiIRXAibv2ily5gQJNAHgZ9SG7"; # start
-  description = "Torben";
-  uid = 1000;
-  group = "users";
-  createHome = true;
-  isNormalUser = true;
-  shell = pkgs.zsh;
-  extraGroups = ["wheel" "networkmanager" "audio" "storage" "input" "video" "docker" "libvirtd" "qemu-libvirtd"];
-};
-# home-manager
-home-manager.users.torben = {
-  home = {
-    stateVersion = "24.05";
-    username = "torben";
-    homeDirectory = "/home/torben";
-    keyboard.layout = "de";
+  # Benutzer
+  users.users.torben = {
+    initialHashedPassword = "$y$j9T$SSQCI4meuJbX7vzu5H.dR.$VUUZgJ4mVuYpTu3EwsiIRXAibv2ily5gQJNAHgZ9SG7"; # start
+    description = "Torben";
+    uid = 1013;
+    group = "users";
+    createHome = true;
+    isNormalUser = true;
+    shell = pkgs.zsh;
+    extraGroups = ["wheel" "networkmanager" "audio" "storage" "input" "video" "docker" "libvirtd" "qemu-libvirtd"];
+  };
+  # home-manager
+  home-manager.users.torben = {
+    home = {
+      stateVersion = "24.05";
+      username = "torben";
+      homeDirectory = "/home/torben";
+      keyboard.layout = "de";
       sessionVariables = {
         EDITOR = "nano";
         VISUAL = "nano";
@@ -37,6 +37,7 @@ home-manager.users.torben = {
         bmon
         dnsutils
         dig
+        bitwarden-desktop
         #git-crypt
         #git-agecrypt
         #httpie
@@ -65,8 +66,8 @@ home-manager.users.torben = {
         vlc
         vscode
         xh #Tool for sending HTTP requests
-        ];
-      };
+      ];
+    };
     fonts.fontconfig.enable = true;
     programs = {
       btop.enable = true;
@@ -86,111 +87,111 @@ home-manager.users.torben = {
           selection = {
             save_to_clipboard = true;
           };
-        scrolling = {
-          history = 50000;
+          scrolling = {
+            history = 50000;
           };
-        font.size = 12;
-        colors.primary = {
-          background = "#222234";
-          foreground = "#fffbf6";
+          font.size = 12;
+          colors.primary = {
+            background = "#222234";
+            foreground = "#fffbf6";
           };
-        colors.normal = {
-          black = "#2e2e2e";
-          red = "#eb4129";
-          green = "#abe047";
-          yellow = "#f6c744";
-          blue = "#47a0f3";
-          magenta = "#7b5cb0";
-          cyan = "#64dbed";
-          white = "#e5e9f0";
+          colors.normal = {
+            black = "#2e2e2e";
+            red = "#eb4129";
+            green = "#abe047";
+            yellow = "#f6c744";
+            blue = "#47a0f3";
+            magenta = "#7b5cb0";
+            cyan = "#64dbed";
+            white = "#e5e9f0";
           };
-        colors.bright = {
-          black = "#565656";
-          red = "#ec5357";
-          green = "#c0e17d";
-          yellow = "#f9da6a";
-          blue = "#49a4f8";
-          magenta = "#a47de9";
-          cyan = "#99faf2";
-          white = "#ffffff";
+          colors.bright = {
+            black = "#565656";
+            red = "#ec5357";
+            green = "#c0e17d";
+            yellow = "#f9da6a";
+            blue = "#49a4f8";
+            magenta = "#a47de9";
+            cyan = "#99faf2";
+            white = "#ffffff";
           };
-        window = {
-          decorations = "none";
-          startup_mode = "Fullscreen";
+          window = {
+            decorations = "none";
+            startup_mode = "Fullscreen";
           };
         };
       };
-    atuin = {
-      enable = true;
+      atuin = {
+        enable = true;
       };
-    #bat = { #broken since 20082024
+      #bat = { #broken since 20082024
       #enable = true;
       #extraPackages = with pkgs.bat-extras; [batdiff batman batgrep batwatch prettybat];
       #};
-    eza = {
-      enable = true;
-      git = true;
-      icons = true;
-      extraOptions = ["--group-directories-first" "--header"];
+      eza = {
+        enable = true;
+        git = true;
+        icons = true;
+        extraOptions = ["--group-directories-first" "--header"];
       };
-    fd = {
-      enable = true;
-      extraOptions = ["--absolute-path" "--no-ignore"];
+      fd = {
+        enable = true;
+        extraOptions = ["--absolute-path" "--no-ignore"];
       };
-    gh = {
-      enable = true;
-      settings.git_protocol = "ssh";
+      gh = {
+        enable = true;
+        settings.git_protocol = "ssh";
       };
-    zsh = {
-      enable = true;
-      autocd = true;
-      autosuggestion.enable = true;
-      syntaxHighlighting.enable = true;
-      historySubstringSearch.enable = true;
-      history = {
-        extended = true;
-        ignoreSpace = true;
-        share = true;
+      zsh = {
+        enable = true;
+        autocd = true;
+        autosuggestion.enable = true;
+        syntaxHighlighting.enable = true;
+        historySubstringSearch.enable = true;
+        history = {
+          extended = true;
+          ignoreSpace = true;
+          share = true;
         };
       };
     };
-  dconf = {
-    enable = true;
-    settings = {
-      "org/gnome/shell" = {
-        disable-user-extensions = false;
-        enabled-extensions = with pkgs.gnomeExtensions; [
-          #app-menu.extensionUuid
-          #places-status-menu.extensionUuid
-          system-monitor.extensionUuid
-          todotxt.extensionUuid
-          #toggle-alacritty.extensionUuid
-          wireguard-vpn-extension.extensionUuid
-          wireless-hid.extensionUuid
-          wifi-qrcode.extensionUuid
+    dconf = {
+      enable = true;
+      settings = {
+        "org/gnome/shell" = {
+          disable-user-extensions = false;
+          enabled-extensions = with pkgs.gnomeExtensions; [
+            #app-menu.extensionUuid
+            #places-status-menu.extensionUuid
+            system-monitor.extensionUuid
+            todotxt.extensionUuid
+            #toggle-alacritty.extensionUuid
+            wireguard-vpn-extension.extensionUuid
+            wireless-hid.extensionUuid
+            wifi-qrcode.extensionUuid
           ];
-        favorite-apps = ["org.gnome.Nautilus.desktop" "librewolf.desktop" "Alacritty.desktop" "org.gnome.Console.desktop" "code.desktop" "org.gnome.Calendar.desktop" "nixos-manual.desktop" "org.gnome.Weather.desktop" "org.gnome.Settings.desktop"];
+          favorite-apps = ["org.gnome.Nautilus.desktop" "librewolf.desktop" "Alacritty.desktop" "org.gnome.Console.desktop" "code.desktop" "org.gnome.Calendar.desktop" "nixos-manual.desktop" "org.gnome.Weather.desktop" "org.gnome.Settings.desktop"];
         };
-      "org/gnome/settings-daemon/plugins/media-keys" = {
-        custom-keybindings = ["/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/" "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/" "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/"];
+        "org/gnome/settings-daemon/plugins/media-keys" = {
+          custom-keybindings = ["/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/" "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/" "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/"];
         };
-      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
-        name = "Alacritty Terminal";
-        command = "alacritty";
-        binding = "<Super>Return";
+        "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+          name = "Alacritty Terminal";
+          command = "alacritty";
+          binding = "<Super>Return";
         };
-      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
-        name = "Editor";
-        command = "gnome-text-editor";
-        binding = "LaunchA";
+        "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
+          name = "Editor";
+          command = "gnome-text-editor";
+          binding = "LaunchA";
         };
-      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2" = {
-        name = "Dateimanager";
-        command = "nautilus";
-        binding = "LaunchB";
+        "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2" = {
+          name = "Dateimanager";
+          command = "nautilus";
+          binding = "LaunchB";
         };
-      "org/gnome/desktop/interface" = {
-        clock-show-weekday = true;
+        "org/gnome/desktop/interface" = {
+          clock-show-weekday = true;
         };
       };
     };
