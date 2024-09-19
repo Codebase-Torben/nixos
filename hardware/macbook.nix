81303dcdfd498c8ca60adde5a68e904c88ab3d04
Codebase-Torben <dauben_01_verehrer@icloud.com>
2024-09-19 17:50:00 +0200

    macbook.nix aktualisieren
    Kamera an

--- hardware/macbook.nix
+++ hardware/macbook.nix
@@ -6,37 +6,51 @@
 }: {
   # Import
   imports = [
-    (modulesPath + "/hardware/network/broadcom-43xx.nix")
-    (modulesPath + "/installer/scan/not-detected.nix")
+    # (modulesPath + "/hardware/network/broadcom-43xx.nix")
+    # (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
+  # nixpkgs
+  nixpkgs = {
+    config.allowUnfree = lib.mkDefault true;
+    hostPlatform = lib.mkDefault "x86_64-linux";
+  };
+
   # Boot
-  boot = {
-    # kernelParams = ["brcmfmac.feature_disable=0x82000"];
-    # kernelParams = ["hid_apple.iso_layout=0"];
-    kernelParams = ["hid_apple.swap_opt_cmd=1"];
+	boot = {
+    blacklistedKernelModules = ["b43" "bcma" "brcmsmac" "ssb"];
+    kernelParams = ["hid_apple.swap_opt_cmd=1" "hid_apple.iso_layout=0" "intel_iommu=on"];
     initrd = {
       availableKernelModules = [
         "applespi"
         "applesmc"
         "spi_pxa2xx_platform"
         "intel_lpss_pci"
+        "kvm-intel"
       ];
     };
   };
 
   # Hardware
   hardware = {
+    enableAllFirmware = lib.mkForce true;
     facetimehd.enable = lib.mkForce true;
-  };
-
-  # Netzwerk
-  networking = {
-    enableB43Firmware = true;
+    graphics.extraPackages = with pkgs; [intel-vaapi-driver intel-ocl intel-media-driver];
+    cpu = {
+      intel = {
+        updateMicrocode = lib.mkForce true;
+        sgx.provision.enable = lib.mkForce false;
+      };
+    };
   };
 
   # Dienste
   services = {
     mbpfan.enable = true;
+  };
+
+  # env
+  environment = {
+    systemPackages = with pkgs; [libsmbios];
   };
 }
