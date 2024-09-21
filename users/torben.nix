5d6b424fbab22e9fbfc6e66cd6690c7eba49e41d
Codebase-Torben <dauben_01_verehrer@icloud.com>
2024-09-21 11:42:00 +0000

    Git per User angelegt.

--- users/torben.nix
+++ users/torben.nix
@@ -79,7 +79,35 @@
       thefuck.enable = true;
       starship.enable = true;
       gh-dash.enable = true;
-      git.enable = true;
+      git = {
+        enable = true;
+        prompt.enable = true;
+        config = {
+          branch.sort = "-committerdate";
+          commit.gpgsign = true;
+          init.defaultBranch = "main";
+          safe.directory = "/etc/nixos";
+          gpg.format = "ssh";
+          user = {
+            email = "torben@nixbook";
+            name = "Git Signing";
+            signingkey = "~/.ssh/schluessel4git.pub";
+          };
+          http = {
+            sslVerify = "true";
+            sslVersion = "tlsv1.3";
+            version = "HTTP/1.1";
+          };
+          protocol = {
+            allow = "never";
+            file.allow = "always";
+            git.allow = "never";
+            ssh.allow = "always";
+            http.allow = "never";
+            https.allow = "always";
+          };
+        };
+      };
       gitui.enable = true;
       lazygit.enable = true;
       home-manager.enable = true;
