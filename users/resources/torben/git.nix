b93f864b1bea012d6983d4ef7d53077c29691309
Codebase-Torben <dauben_01_verehrer@icloud.com>
2024-09-22 13:50:01 +0000

    Userbased things

---
+++ users/resources/torben/git.nix
@@ -0,0 +1,39 @@
+{
+  config,
+  pkgs
+  ...
+}: {
+
+  # Git Konfiguration
+  programs = {
+    git = {
+      enable = true;
+      prompt.enable = true;
+      config = {
+        branch.sort = "-committerdate";
+        commit.gpgsign = true;
+        init.defaultBranch = "main";
+        safe.directory = "/etc/nixos";
+        gpg.format = "ssh";
+        user = {
+          email = "torben@nixbook";
+          name = "Git Signing";
+          signingkey = "~/.ssh/schluessel4git.pub";
+        };
+      http = {
+        sslVerify = "true";
+        sslVersion = "tlsv1.3";
+        version = "HTTP/1.1";
+        };
+        protocol = {
+          allow = "never";
+          file.allow = "always";
+          git.allow = "never";
+          ssh.allow = "always";
+          http.allow = "never";
+          https.allow = "always";
+        };
+      };
+    };
+  };
+}
