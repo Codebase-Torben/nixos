0920332f6f7295609e91ac2aaec49b515135f468
Codebase-Torben <dauben_01_verehrer@icloud.com>
2024-09-22 16:53:35 +0000

    Safe Firefox

--- users/resources/torben/firefox.nix
+++ users/resources/torben/firefox.nix
@@ -31,7 +31,7 @@
         DisableAccounts = true;
         DisableFirefoxScreenshots = true;
         OverrideFirstRunPage = "https://github.com/codebase-torben/nixos";
-        OverridePostUpdatePage = "";
+        OverridePostUpdatePage = "https://github.com/codebase-torben/nixos";
         DontCheckDefaultBrowser = true;
         DisplayBookmarksToolbar = "newtab"; # alternatives: "always" or "never"
         DisplayMenuBar = "default-off"; # alternatives: "always", "never" or "default-on"
@@ -42,10 +42,10 @@
         ExtensionSettings = {
           "*".installation_mode = "blocked"; # blocks all addons except the ones specified below
           # uBlock Origin:
-          # "uBlock0@raymondhill.net" = {
-           # install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-           # installation_mode = "force_installed";
-          # };
+          "uBlock0@raymondhill.net" = {
+           install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+           installation_mode = "force_installed";
+          };
           # Bitwarden:
           "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
             install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
