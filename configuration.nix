7ebe717aa7f1d17bb96e96debbbaa41525b39555
Codebase-Torben <dauben_01_verehrer@icloud.com>
2024-09-30 17:09:27 +0000

    ext4 in kernel

--- configuration.nix
+++ configuration.nix
@@ -78,7 +78,7 @@
     blacklistedKernelModules = ["ax25" "netrom" "rose" "affs" "bfs" "befs" "freevxfs" "f2fs" "hpfs" "jfs" "minix" "nilfs2" "omfs" "qnx4" "qnx6" "sysv"];
     kernelPackages = pkgs.linuxPackages_latest;
     kernelParams = ["page_alloc.shuffle=1" "ipv6.disable=1"];
-    kernelModules = ["vfat" "exfat"];
+    kernelModules = ["vfat" "exfat" "ext4"];
     readOnlyNixStore = lib.mkForce true;
     initrd = {
       systemd.enable = lib.mkForce false;
