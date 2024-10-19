6da63741b370cfcc16e60699dcecc2659663f790
Codebase-Torben <dauben_01_verehrer@icloud.com>
2024-10-19 19:09:16 +0200

    Docker eingerichtet

--- modules/docker.nix
+++ modules/docker.nix
@@ -18,41 +18,37 @@
       containers = {
         speed = {
           image = "openspeedtest/latest:latest";
-          ports = ["0.0.0.0:8181:3000"];
+          ports = ["0.0.0.0:7891:3000"];
           #environment = {
             #SET_SERVER_NAME = "name.service.lan";
           #};
         };
         yo = {
           image = "jhaals/yopass:latest";
-          cmd = ["--address=0.0.0.0" "--port=8282" "--metrics-port=9144" "--database=memcached" "--memcached=localhost:11211"];
+          cmd = ["--address=0.0.0.0" "--port=7892" "--metrics-port=9144" "--database=memcached" "--memcached=localhost:11211"];
           extraOptions = ["--network=host"];
         };
         status = {
           image = "adamboutcher/statping-ng:latest";
-          ports = ["0.0.0.0:8383:8080"];
+          ports = ["0.0.0.0:7893:4000"];
         };
         chef = {
           image = "ghcr.io/gchq/cyberchef:latest";
-          ports = ["0.0.0.0:8484:80"];
+          ports = ["0.0.0.0:7894:80"];
         };
-        #nocdb = {
-          #image = "nocdb/nocdb";
-          #ports = ["0.0.0.0:8484:80"];
-        #};
-        #whoogle = {
-          #image = "benbusby/whoogle-search:latest";
-          #ports = ["0.0.0.0:8080:8080"];
-          #environment = {
-            #EXPOSE_PORT = "8080";
-            #WHOOGLE_MINIMAL = "1";
-            #WHOOGLE_RESULTS_PER_PAGE = "50";
-            #WHOOGLE_CONFIG_LANGUAGE = "en";
-            #WHOOGLE_CONFIG_SEARCH_LANGUAGE = "en";
-            #WHOOGLE_CONFIG_SAFE = "1";
-            #WHOOGLE_CONFIG_URL = "http://localhost:8080";
-          #};
-        #};
+        whoogle = {
+          image = "benbusby/whoogle-search:latest";
+          ports = ["0.0.0.0:8080:8080"];
+          environment = {
+            EXPOSE_PORT = "8080";
+            WHOOGLE_MINIMAL = "1";
+            WHOOGLE_RESULTS_PER_PAGE = "200";
+            WHOOGLE_CONFIG_LANGUAGE = "de";
+            WHOOGLE_CONFIG_SEARCH_LANGUAGE = "de";
+            WHOOGLE_CONFIG_SAFE = "1";
+            WHOOGLE_CONFIG_URL = "http://localhost:8080";
+          };
+        };
       };
     };
   };
