98797a09f199ed566448fa8fc98fb7610aa637bc
Codebase-Torben <dauben_01_verehrer@icloud.com>
2024-12-18 20:20:28 +0100

    git.nix aktualisieren

--- users/resources/torben/git.nix
+++ users/resources/torben/git.nix
@@ -3,37 +3,23 @@
   pkgs,
   ...
 }: {
-
   # Git Konfiguration
   programs = {
     git = {
-      enable = true;
-      prompt.enable = true;
-      config = {
-        branch.sort = "-committerdate";
-        commit.gpgsign = true;
-        init.defaultBranch = "main";
-        safe.directory = "/home/torben/safegit";
-        #safe.directory = "/etc/nixos";
-        gpg.format = "ssh";
-        user = {
-          email = "torben@nixbook";
-          name = "Git Signing";
-          signingkey = "~/.ssh/schluessel4git.pub";
-        };
-      http = {
-        sslVerify = "true";
-        sslVersion = "tlsv1.3";
-        version = "HTTP/1.1";
-        };
-        protocol = {
-          allow = "never";
-          file.allow = "always";
-          git.allow = "never";
-          ssh.allow = "always";
-          http.allow = "never";
-          https.allow = "always";
-        };
+      userName = lib.mkForce "Git Signing";
+      userEmail = lib.mkForce "torben@nixbook";
+      signing = {
+        signByDefault = lib.mkForce false;
+        key = lib.mkForce "~/.ssh/schluessel4git.pub";
+      };
+    extraConfig = {
+      protocol = {
+        allow = "never";
+        file.allow = "always";
+        git.allow = "never";
+        ssh.allow = "always";
+        http.allow = "never";
+        https.allow = "never";
       };
     };
   };
