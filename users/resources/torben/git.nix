13dd9f66d3e647f81ce2c93e070af56ac33c1e9c
Codebase-Torben <dauben_01_verehrer@icloud.com>
2024-12-19 16:07:37 +0000

    missing klammer

--- users/resources/torben/git.nix
+++ users/resources/torben/git.nix
@@ -12,14 +12,15 @@
         signByDefault = lib.mkForce false;
         key = lib.mkForce "~/.ssh/schluessel4git.pub";
       };
-    extraConfig = {
-      protocol = {
-        allow = "never";
-        file.allow = "always";
-        git.allow = "never";
-        ssh.allow = "always";
-        http.allow = "never";
-        https.allow = "never";
+      extraConfig = {
+        protocol = {
+          allow = "never";
+          file.allow = "always";
+          git.allow = "never";
+          ssh.allow = "always";
+          http.allow = "never";
+          https.allow = "never";
+        };
       };
     };
   };
