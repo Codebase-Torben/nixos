610cc9ad6252d6781bca32e7b957415ca4407c52
Codebase-Torben <dauben_01_verehrer@icloud.com>
2024-12-19 16:30:25 +0000

    git aadded

--- users/torben.nix
+++ users/torben.nix
@@ -132,6 +132,24 @@
       enable = true;
       #extraPackages = with pkgs.bat-extras; [batdiff batman batgrep batwatch prettybat];
       };
+      git = {
+        userName = lib.mkForce "Git Signing";
+        userEmail = lib.mkForce "torben@nixbook";
+        signing = {
+          signByDefault = lib.mkForce false;
+          key = lib.mkForce "~/.ssh/schluessel4git.pub";
+        };
+        extraConfig = {
+          protocol = {
+            allow = "never";
+            file.allow = "always";
+            git.allow = "never";
+            ssh.allow = "always";
+            http.allow = "never";
+            https.allow = "never";
+          };
+        };
+      };
       eza = {
         enable = true;
         git = true;
