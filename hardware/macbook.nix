a373ee8bf327cbcffe85c0349749c0a1daadc924
Codebase-Torben <dauben_01_verehrer@icloud.com>
2024-12-19 22:38:42 +0000

    macbook.nix new 24.11 needs

--- hardware/macbook.nix
+++ hardware/macbook.nix
@@ -6,31 +6,24 @@
   ...
 }: {
   # Bootvorgang
-  boot = {
-    blacklistedKernelModules = ["b43" "bcma" "brcmsmac" "ssb"];
-    kernelParams = lib.mkForce ["hid_apple.swap_opt_cmd=1" "hid_apple.iso_layout=0" "intel_iommu=on"];
-    initrd = {
-      availableKernelModules = lib.mkForce [
-        "applespi"
-        "applesmc"
-        "spi_pxa2xx_platform"
-        "intel_lpss_pci"
-        "kvm-intel"
-      ];
-    };
-  };
+  #boot = {
+    #blacklistedKernelModules = ["b43" "bcma" "brcmsmac" "ssb"];
+    #kernelParams = lib.mkForce ["hid_apple.swap_opt_cmd=1" "hid_apple.iso_layout=0" "intel_iommu=on"];
+    #initrd = {
+      #availableKernelModules = lib.mkForce [
+        #"applespi"
+        #"applesmc"
+        #"spi_pxa2xx_platform"
+        #"intel_lpss_pci"
+        #"kvm-intel"
+      #];
+    #};
+  #};
 
   # Hardware
   hardware = {
-    enableAllFirmware = lib.mkForce true;
     facetimehd.enable = lib.mkForce false;
     graphics.extraPackages = with pkgs; [intel-vaapi-driver intel-ocl intel-media-driver];
-    cpu = {
-      intel = {
-        updateMicrocode = lib.mkForce true;
-        sgx.provision.enable = lib.mkForce false;
-      };
-    };
   };
 
   # Dienste
@@ -38,7 +31,7 @@
     mbpfan.enable = lib.mkForce true;
   };
 
-  # env
+  # Env
   environment = {
     systemPackages = with pkgs; [libsmbios];
   };
