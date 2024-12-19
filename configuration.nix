f17e35e8a5d50dbfe42b50f98ebe8d261ff59eaf
Codebase-Torben <dauben_01_verehrer@icloud.com>
2024-12-19 23:45:45 +0000

    restrict_eval false

--- configuration.nix
+++ configuration.nix
@@ -38,7 +38,7 @@
       sandbox-build-dir = "/build";
       sandbox-fallback = lib.mkForce false;
       trace-verbose = true;
-      restrict-eval = lib.mkForce true;
+      restrict-eval = lib.mkForce false;
       require-sigs = lib.mkForce true;
       preallocate-contents = true;
       allowed-uris = [
