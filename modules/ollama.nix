210ca9096cb71f1490d0061518a1657324fb9e18
Codebase-Torben <dauben_01_verehrer@icloud.com>
2025-01-14 09:27:19 +0100

    Update ollama.nix

--- modules/ollama.nix
+++ modules/ollama.nix
@@ -1,17 +1,28 @@
 {config, ...}: {
-  # Dienst
+  # Dienste
   services = {
     ollama = {
       enable = true;
-      host = "127.0.0.1";
-      port = "11434";
-      openFirewall = false;
-      sandbox = true;
+      # host = "127.0.0.1";
+      # port = 11434;
+      # openFirewall = false;
+      # user = "ollama";
+      # group = "ollama";
+      # acceleration = false;
       # environmentVariables = [];
       # home = "";
+      # models = "";
       # loadModels = [];
       # rocmOverrideGfx = "";
-      # writeablePath = [];
+    };
+  };
+
+  # env
+  environment = {
+    shellAliases = {
+      ollama-commit = "/home/me/.npm-packages/bin/ollama-commit --language de --api http://localhost:11434 --model mistral";
+      tlm = "go run github.com/yusufcanb/tlm@latest $*";
+      gollama = "go run github.com/sammcj/gollama@latest $*";
     };
   };
 }
