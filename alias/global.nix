657926b51baa1e49da6ecbefe51a46f0aed62e12
Codebase-Torben <dauben_01_verehrer@icloud.com>
2025-01-19 15:40:38 +0000

    New save your a** aliases
    get a compressed copy of all to the mounted usb device

--- alias/global.nix
+++ alias/global.nix
@@ -6,16 +6,8 @@
   environment = {
     shellAliases = {
       # Aliase
-      etcnix = "cd / && cd /etc/nixos";
-      homenix = "cd / && cd /home/torben/Dokumente/nixos";
-      savehome = ''
-        export USBSTORE="$(ls /var/run/media/torben)" ;\
-        export ZTSTMPL="$(date '+%d-%m-%Y-%H-%M')" ;\
-        tar czfv /var/run/media/torben/$USBSTORE/NixOS/home_$ZTSTMPL.tar.gz --directory=/home/torben . '';
-      savevarlib = ''
-        export USBSTORE="$(ls /var/run/media/torben)" ;\
-        export ZTSTMPL="$(date '+%d-%m-%Y-%H-%M')" ;\
-        sudo tar czfv /var/run/media/torben/$USBSTORE/NixOS/varlib_$ZTSTMPL.tar.gz --directory=/var/lib . '';
+
+      # Bauen
       "testbuild" = ''
         etcnix ;\
         export HNAME="$(hostname)" ;\
@@ -37,6 +29,8 @@
         sudo nom build .#nixosConfigurations.$HNAME.config.system.build.toplevel ;\
         sudo rm -f result ;\
         sudo nixos-rebuild boot --flake "/etc/nixos/.#$HNAME" -p "NixOS-Luna-vom-$ZTSTMPL" '';
+
+      # Nix Store
       "storecheck" = ''             
         etcnix ;\
         sudo nix-store -v --gc ;\
@@ -47,6 +41,8 @@
         sudo nix-store -v --verify --check-contents --repair ;\
         sudo nix flake check -v ;\
         sudo nix flake update -v '';
+
+      # Profile
       "cleanix" = ''
         etcnix ;\
         sudo nix-env --delete-generations --profile /nix/var/nix/profiles/system 6d ;\
@@ -63,8 +59,23 @@
         sudo nix-store --gc ;\
         sudo nix-store --optimise ;\
         neubauen '';
+
+      # Sicherung
+      "savehome" = ''
+        export USBSTORE="$(ls /var/run/media/torben)" ;\
+        export ZTSTMPL="$(date '+%d-%m-%Y-%H-%M')" ;\
+        tar czfv /var/run/media/torben/$USBSTORE/NixOS/home_$ZTSTMPL.tar.gz --directory=/home/torben . '';
+      "savevarlib" = ''
+        export USBSTORE="$(ls /var/run/media/torben)" ;\
+        export ZTSTMPL="$(date '+%d-%m-%Y-%H-%M')" ;\
+        sudo tar czfv /var/run/media/torben/$USBSTORE/NixOS/varlib_$ZTSTMPL.tar.gz --directory=/var/lib . '';
+
+      # Quality of life
+      b = "btop";
       ed = "sudo nano";
-      termshark = "sudo termshark";
+      etcnix = "cd / && cd /etc/nixos";
+      homenix = "cd / && cd /home/torben/Dokumente/nixos";
+      h = "htop --tree --highlight-changes";
       l = "ls -la";
       ll = "eza --all --long --total-size --group-directories-first --header --git --git-repos --sort=filename";
       la = "eza --all --long --total-size --group-directories-first --header --git --git-repos --sort=size";
@@ -72,13 +83,14 @@
       lo = "eza --all --long --total-size --group-directories-first --header --git --git-repos --sort=filename --octal-permissions";
       li = "eza --all --long --total-size --group-directories-first --header --git --git-repos --sort=inode --inode";
       meow = "kittysay";
-      b = "btop";
-      h = "htop --tree --highlight-changes";
+      termshark = "sudo termshark";
       time = "timedatectl && chronyc tracking && chronyc activity";
-      "logbuch" = "journalctl --since='30 min ago' -u $(systemctl list-units --type=service | fzf | sed 's/●/ /g' | cut --fields 3 --delimiter ' ')";
-      "starter" = "sudo systemctl start $(systemctl list-units --type=service --all | fzf | sed 's/●/ /g' | cut --fields 3 --delimiter ' ')";
-      "stopper" = "sudo systemctl stop $(systemctl list-units --type=service | fzf | sed 's/●/ /g' | cut --fields 3 --delimiter ' ')";
-      "neustarter" = "sudo systemctl restart $(systemctl list-units --type=service | fzf | sed 's/●/ /g' | cut --fields 3 --delimiter ' ')";
+
+      # Services
+      logbuch = "journalctl --since='30 min ago' -u $(systemctl list-units --type=service | fzf | sed 's/●/ /g' | cut --fields 3 --delimiter ' ')";
+      neustarter = "sudo systemctl restart $(systemctl list-units --type=service | fzf | sed 's/●/ /g' | cut --fields 3 --delimiter ' ')";
+      starter = "sudo systemctl start $(systemctl list-units --type=service --all | fzf | sed 's/●/ /g' | cut --fields 3 --delimiter ' ')";
+      stopper = "sudo systemctl stop $(systemctl list-units --type=service | fzf | sed 's/●/ /g' | cut --fields 3 --delimiter ' ')";
     };
   };
 }
