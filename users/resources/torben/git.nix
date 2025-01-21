2eb08bf1c9fd94f03cf63cf67cd9940cde7116bc
Git Signing <torben@nixbook>
2025-01-21 17:31:28 +0100

    Luna 1.0.8 Git Edition

--- users/resources/torben/git.nix
+++ users/resources/torben/git.nix
@@ -1,33 +1,36 @@
 {
   config,
   pkgs,
+  lib,
   ...
 }: {
   # Git Konfiguration
-  programs = {
-    gh-dash.enable = true;
-    gitui.enable = true;
-    #lazygit.enable = true;
-    gh = {
-      enable = true;
-      settings.git_protocol = "ssh";
-    };
-    git = {
-      enable = true;
-      userName = lib.mkForce "Git Signing";
-      userEmail = lib.mkForce "torben@nixbook";
-      signing = {
-        signByDefault = lib.mkForce false;
-        key = lib.mkForce "~/.ssh/schluessel4git.pub";
+  home-manager.users.torben = {
+    programs = {
+      gh-dash.enable = true;
+      gitui.enable = true;
+      #lazygit.enable = true;
+      gh = {
+        enable = true;
+        settings.git_protocol = "ssh";
       };
-      extraConfig = {
-        protocol = {
-          allow = "never";
-          file.allow = "always";
-          git.allow = "never";
-          ssh.allow = "always";
-          http.allow = "never";
-          https.allow = "never";
+      git = {
+        enable = true;
+        userName = lib.mkForce "Git Signing";
+        userEmail = lib.mkForce "torben@nixbook";
+        signing = {
+          signByDefault = lib.mkForce false;
+          key = lib.mkForce "~/.ssh/schluessel4git.pub";
+        };
+        extraConfig = {
+          protocol = {
+            allow = "never";
+            file.allow = "always";
+            git.allow = "never";
+            ssh.allow = "always";
+            http.allow = "never";
+            https.allow = "never";
+          };
         };
       };
     };
