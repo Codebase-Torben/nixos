e068484fe731a62f35c606a313135d5b572718ee
Codebase-Torben <dauben_01_verehrer@icloud.com>
2025-01-23 18:16:56 +0000

    openweb-ui.nix aktualisieren

--- modules/openweb-ui.nix
+++ modules/openweb-ui.nix
@@ -1,22 +1,11 @@
 {config, ...}: {
-  ##################
-  #-=# SERVICES #=-#
-  ##################
+  # Dienste
   services = {
     open-webui = {
       enable = true;
       host = "127.0.0.1";
       port = 6161;
       openFirewall = false;
-    };
-  };
-
-  ####################
-  #-=# NETWORKING #=-#
-  ####################
-  networking = {
-    firewall = {
-      allowedTCPPorts = [80 443];
     };
   };
 }
